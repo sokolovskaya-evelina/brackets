3751a67e32fd2dc451c92c5bdbea31578cc08390
Evelina Sokolovskaya <sokolovskayaevi@gmail.com>
2020-10-07 21:05:13 +0300

    feat: add the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let found = 1;
+  while(found == 1) {
+      let oldStr = str;
+      bracketsConfig.map(n => str = str.replace(n[0] + n[1], ''));
+      if (oldStr === str) { found = 0; }
+  }
+  if (str.length === 0) {
+    return true
+  } else {
+    return false
+  }
 }
